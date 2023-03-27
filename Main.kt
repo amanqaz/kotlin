88c82b8cc34e743b7d81a52d5e6469bfb8d8b253
Aman Kumar Chaurasia <amanisyoyo@gmail.com>
2023-03-28 01:09:10 +0530

    first

---
+++ Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    println("first program" )
+    val intArrayOf = intArrayOf(1, 2, 3, 4, 5)
+    for (int i=0 i<intArrayOf.size)
+
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
